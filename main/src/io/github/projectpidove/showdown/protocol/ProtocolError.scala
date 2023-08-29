9d214aa890ff34bb893113ee1406f3f4359a3b08
Iltotore <rafbodaha@gmail.com>
2023-08-29 18:50:05 +0200

    fix: Merge conflict with main

--- main/src/io/github/projectpidove/showdown/protocol/ProtocolError.scala
+++ main/src/io/github/projectpidove/showdown/protocol/ProtocolError.scala
@@ -3,29 +3,34 @@ package io.github.projectpidove.showdown.protocol
 /**
  * A protocol-bound error
  */
-enum ProtocolError:
+enum ProtocolError(message: String) extends Throwable(message):
 
   /**
    * The input has been exhausted.
    */
-  case InputExhausted(data: String, length: Int)
+  case InputExhausted(data: String, length: Int) extends ProtocolError(s"Input exhausted. Length: $length")
 
   /**
    * Tried to decode an invalid input.
    */
-  case InvalidInput(input: String, message: String)
+  case InvalidInput(input: String, message: String) extends ProtocolError(s"Invalid input: $input, $message")
 
   /**
    * The input is in invalid format.
    */
-  case InvalidFormat(message: String)
+  case InvalidFormat(message: String) extends ProtocolError(s"Invalid format: $message")
 
   /**
    * Miscellaneous error.
    */
-  case Miscellaneous(message: String)
+  case Miscellaneous(message: String) extends ProtocolError(message)
 
   /**
    * The ProtocolError has been caused by a thrown error.
    */
-  case Thrown(cause: Throwable)
+  case Thrown(cause: Throwable) extends ProtocolError(cause.getMessage)
+
+  /**
+   * The connection has been closed.
+   */
+  case ConnectionClosed extends ProtocolError("Connection closed")
