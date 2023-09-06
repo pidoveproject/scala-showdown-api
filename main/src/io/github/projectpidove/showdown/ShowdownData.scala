57846cf056a2cee64ba616f5af43d65c8a76e506
Iltotore <rafbodaha@gmail.com>
2023-09-06 14:30:27 +0200

    feat: Add PM support

--- main/src/io/github/projectpidove/showdown/ShowdownData.scala
+++ main/src/io/github/projectpidove/showdown/ShowdownData.scala
@@ -1,8 +1,8 @@
 package io.github.projectpidove.showdown
 
 import io.github.projectpidove.showdown.protocol.server.{GlobalMessage, RoomBoundMessage, ServerMessage}
-import io.github.projectpidove.showdown.room.{JoinedRoom, RoomId}
-import io.github.projectpidove.showdown.user.LoggedUser
+import io.github.projectpidove.showdown.room.{ChatContent, ChatMessage, JoinedRoom, RoomId}
+import io.github.projectpidove.showdown.user.{LoggedUser, Username}
 
 /**
  * The state of the Showdown connection.
@@ -21,6 +21,8 @@ case class ShowdownData(
     formatCategories: List[FormatCategory],
     joinedRooms: Map[RoomId, JoinedRoom]
 ):
+  
+  def isLoggedAs(name: Username): Boolean = loggedUser.exists(_.name == name)
 
   def getJoinedRoomOrEmpty(id: RoomId): JoinedRoom = joinedRooms.getOrElse(id, JoinedRoom.empty(id))
 
@@ -33,11 +35,25 @@ case class ShowdownData(
   def update(message: ServerMessage): ShowdownData = message match
     case GlobalMessage.UserCount(count)                          => this.copy(userCount = Some(count))
     case GlobalMessage.ChallStr(challStr)                        => this.copy(challStr = Some(challStr))
-    case GlobalMessage.UpdateUser(user, named, avatar, settings) => this.copy(loggedUser = Some(LoggedUser(user.name, avatar, !named, settings)))
-    case GlobalMessage.UpdateSearch(search)                      => this.copy(gameSearch = search)
-    case GlobalMessage.Formats(categories)                       => this.copy(formatCategories = categories)
-    case RoomBoundMessage(id, message)                           => this.copy(joinedRooms = joinedRooms.updated(id, getJoinedRoomOrEmpty(id).update(message)))
-    case _                                                       => this
+    case GlobalMessage.UpdateUser(user, named, avatar, settings) =>
+      val result = loggedUser match
+        case Some(value) => value.copy(name = user.name, avatar = avatar, isGuest = !named, settings = settings)
+        case None => LoggedUser(user.name, avatar, !named, settings, Map.empty)
+
+      this.copy(loggedUser = Some(result))
+    case GlobalMessage.PrivateMessage(sender, receiver, message) if isLoggedAs(sender.name) || isLoggedAs(receiver.name) =>
+      val key =
+        if isLoggedAs(sender.name) then receiver
+        else sender
+
+      val user = loggedUser.get
+      val updatedChat = user.getPrivateChat(key).withChatMessage(ChatMessage.Sent(sender, message))
+
+      this.copy(loggedUser = Some(user.withPrivateChat(key, updatedChat)))
+    case GlobalMessage.UpdateSearch(search) => this.copy(gameSearch = search)
+    case GlobalMessage.Formats(categories) => this.copy(formatCategories = categories)
+    case RoomBoundMessage(id, message) => this.copy(joinedRooms = joinedRooms.updated(id, getJoinedRoomOrEmpty(id).update(message)))
+    case _ => this
 
 object ShowdownData:
 
--- main/src/io/github/projectpidove/showdown/user/LoggedUser.scala
+++ main/src/io/github/projectpidove/showdown/user/LoggedUser.scala
@@ -1,5 +1,8 @@
 package io.github.projectpidove.showdown.user
 
+import io.github.projectpidove.showdown.protocol.server.ServerMessage
+import io.github.projectpidove.showdown.room.RoomChat
+
 /**
  * Represent a logged-in user. Typically the current user when connecting to Showdown.
  *
@@ -8,4 +11,14 @@ package io.github.projectpidove.showdown.user
  * @param isGuest whether this user is a guest or has a registered account
  * @param settings this user's settings
  */
-case class LoggedUser(name: Username, avatar: AvatarName, isGuest: Boolean, settings: UserSettings)
+case class LoggedUser(
+  name: Username,
+  avatar: AvatarName,
+  isGuest: Boolean,
+  settings: UserSettings,
+  privateMessages: Map[User, RoomChat]
+):
+  
+  def getPrivateChat(user: User): RoomChat = privateMessages.getOrElse(user, RoomChat.empty)
+
+  def withPrivateChat(user: User, chat: RoomChat): LoggedUser = this.copy(privateMessages = privateMessages.updated(user, chat))
