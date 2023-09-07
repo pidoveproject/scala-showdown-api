14d1836a4e021103a74a4676268751e4503887c9
Iltotore <rafbodaha@gmail.com>
2023-09-07 09:18:17 +0200

    feat: Track users' details

--- main/src/io/github/projectpidove/showdown/ShowdownData.scala
+++ main/src/io/github/projectpidove/showdown/ShowdownData.scala
@@ -1,5 +1,7 @@
 package io.github.projectpidove.showdown
 
+import io.github.projectpidove.showdown.protocol.server.query.ResponseContent.UserDetails
+import io.github.projectpidove.showdown.protocol.server.query.{ResponseContent, UserInfo}
 import io.github.projectpidove.showdown.protocol.server.{GlobalMessage, RoomBoundMessage, ServerMessage}
 import io.github.projectpidove.showdown.room.{ChatContent, ChatMessage, JoinedRoom, RoomId}
 import io.github.projectpidove.showdown.user.{LoggedUser, Username}
@@ -12,6 +14,8 @@ import io.github.projectpidove.showdown.user.{LoggedUser, Username}
  * @param loggedUser the currently logged-in user
  * @param gameSearch the current state of game matchmaking
  * @param formatCategories the formats and categories available on the connected server
+ * @param joinedRooms the room the logged user is currently connected to
+ * @param userDetails the details of other users
  */
 case class ShowdownData(
     userCount: Option[Count],
@@ -19,12 +23,33 @@ case class ShowdownData(
     loggedUser: Option[LoggedUser],
     gameSearch: GameSearch,
     formatCategories: List[FormatCategory],
-    joinedRooms: Map[RoomId, JoinedRoom]
+    joinedRooms: Map[RoomId, JoinedRoom],
+    userDetails: Map[String, UserInfo]
 ):
-  
+
+  /**
+   * Check if the client is logged with the given name.
+   *
+   * @param name the name to check
+   * @return whether the current user has the name `name` or not
+   */
   def isLoggedAs(name: Username): Boolean = loggedUser.exists(_.name == name)
 
+  /**
+   * Get the room information from an id.
+   *
+   * @param id the id of the room to get
+   * @return the room with the given id or an empty instance if it does not exist
+   */
   def getJoinedRoomOrEmpty(id: RoomId): JoinedRoom = joinedRooms.getOrElse(id, JoinedRoom.empty(id))
+
+  /**
+   * Get the details of a user.
+   *
+   * @param id the id of the user
+   * @return the details of the user with the given id or `None` if absent
+   */
+  def getUserDetails(id: String): Option[UserInfo] = userDetails.get(id)
 
   /**
    * Update this data according to the passed server event/message.
@@ -52,6 +77,7 @@ case class ShowdownData(
       this.copy(loggedUser = Some(user.withPrivateChat(key, updatedChat)))
     case GlobalMessage.UpdateSearch(search) => this.copy(gameSearch = search)
     case GlobalMessage.Formats(categories) => this.copy(formatCategories = categories)
+    case GlobalMessage.QueryResponse(ResponseContent.UserDetails(info)) => this.copy(userDetails = userDetails.updated(info.id, info))
     case RoomBoundMessage(id, message) => this.copy(joinedRooms = joinedRooms.updated(id, getJoinedRoomOrEmpty(id).update(message)))
     case _ => this
 
@@ -66,5 +92,6 @@ object ShowdownData:
     loggedUser = None,
     gameSearch = GameSearch.empty,
     formatCategories = List.empty,
-    joinedRooms = Map.empty
+    joinedRooms = Map.empty,
+    userDetails = Map.empty
   )
