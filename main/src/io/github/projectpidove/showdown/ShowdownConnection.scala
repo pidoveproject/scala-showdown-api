d56ebcddb8db25069ab0e7453cdc83e4b17d563c
Rayan Desfossez <115161783+rayandfz@users.noreply.github.com>
2023-12-03 16:23:09 +0100

    feat: Add utility methods (#36)
    Closes #20

--- main/src/io/github/projectpidove/showdown/ShowdownConnection.scala
+++ main/src/io/github/projectpidove/showdown/ShowdownConnection.scala
@@ -1,9 +1,9 @@
 package io.github.projectpidove.showdown
 
 import io.github.projectpidove.showdown.protocol.LoginResponse
-import io.github.projectpidove.showdown.protocol.client.{ClientMessage, GlobalCommand}
+import io.github.projectpidove.showdown.protocol.client.{ClientMessage, GlobalCommand, OptionCommand}
 import io.github.projectpidove.showdown.protocol.server.ServerMessage
-import io.github.projectpidove.showdown.room.RoomId
+import io.github.projectpidove.showdown.room.{ChatContent, RoomId}
 import io.github.projectpidove.showdown.user.Username
 
 /**
@@ -37,6 +37,77 @@ trait ShowdownConnection[Frame, Cmd[_]]:
   def sendMessage(message: ClientMessage): Cmd[Unit]
 
   /**
+   * Send a private message to another player.
+   *
+   * @param recipient the recipient of the message
+   * @param message the message to send
+   */
+  def sendPrivateMessage(recipient: Username, message: ChatContent): Cmd[Unit] =
+    sendMessage(GlobalCommand.Msg(recipient, message))
+
+  /**
+   * Disconnect the user
+   */
+  def logout(): Cmd[Unit] =
+    sendMessage(GlobalCommand.LogOut)
+
+  /**
+   * Accept a challenge
+   */
+  def acceptChallenge(): Cmd[Unit] =
+    sendMessage(OptionCommand.UnblockChallenges)
+
+  /**
+   * Challenge another user
+   *
+   * @param user the user to challenge
+   * @param format the format of the battle
+   */
+  def challengeUser(user: Username, format: FormatName): Cmd[Unit] =
+    sendMessage(GlobalCommand.Challenge(user, format))
+
+  /**
+   * Search for a battle
+   *
+   * @param format the format to search for
+   */
+  def searchBattle(format: FormatName): Cmd[Unit] =
+    sendMessage(GlobalCommand.Search(format))
+
+  /**
+   * Cancel the match search.
+   */
+  def cancelSearch(): Cmd[Unit] = sendMessage(GlobalCommand.CancelSearch)
+
+  /**
+   * Rename current user.
+   *
+   * @param name the new name to take
+   */
+  def rename(name: Username): Cmd[Unit] = sendMessage(OptionCommand.Nick(Some(name)))
+
+  /**
+   * Reset current user's name.
+   */
+  def resetName(): Cmd[Unit] = sendMessage(OptionCommand.Nick(None))
+
+  /**
+   * Join a room.
+   *
+   * @param room the id of the room to join
+   */
+  def joinRoom(room: RoomId): Cmd[Unit] =
+    sendMessage(GlobalCommand.Join(room))
+
+  /**
+   * Leave a room
+   *
+   * @param room the id of the room to leave
+   */
+  def leaveRoom(room: RoomId): Cmd[Unit] =
+    sendMessage(GlobalCommand.Leave(Some(room)))
+
+  /**
    * Disconnect from the server.
    */
   def disconnect(): Cmd[Unit]
@@ -59,7 +130,7 @@ trait ShowdownConnection[Frame, Cmd[_]]:
 
   /**
    * Login as guest.
-   * 
+   *
    * @param name the name to take in game
    */
   def loginGuest(name: Username): Cmd[String]
