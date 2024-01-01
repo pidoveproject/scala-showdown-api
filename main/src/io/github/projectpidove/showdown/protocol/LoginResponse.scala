5f658f9af53988cb342d715efec812e1dfea9c12
Iltotore <rafbodaha@gmail.com>
2024-01-01 10:09:16 +0100

    refactor: Rename package from projectpidove to pidoveproject

--- main/src/io/github/projectpidove/showdown/protocol/LoginResponse.scala
+++
@@ -1,9 +0,0 @@
-package io.github.projectpidove.showdown.protocol
-
-import zio.json.*
-
-case class LoginResponse(
-                          @jsonField("actionsuccess") actionSuccess: Boolean,
-                          assertion: String,
-                          @jsonField("curuser") currentUser: CurrentUser
-                        )derives JsonDecoder
